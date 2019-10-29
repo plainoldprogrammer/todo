0194fb71097490c74ac42fd409120bf6d0629466
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-10-29 17:06:44 -0500

    creating the entry point

---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+ * Name:		PlainOldProgrammer
+ * Start Date:	Tuesday 29 October 2019
+ * Description:	Entry point of the command line utility.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
+
