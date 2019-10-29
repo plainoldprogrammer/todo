396350d6955ebe9e8d680a8fcff14170477b758d
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-10-29 17:25:17 -0500

    defining the main loop to process the command line arguments

--- main.c
+++ main.c
@@ -5,8 +5,19 @@
  *
  */
 
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
+	printf("Number or arguments: %d\n", argc);
+
+	int i = 0;
+	while (i < argc)
+	{
+		printf("argv[%d]: %s\n", i, argv[i]);
+		i++;
+	}
+
 	return 0;
 }
 
